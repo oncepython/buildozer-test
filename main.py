639ce370ba519ecec8db1e6a494641bc5bd9ab5b
oncepython <hsxb2024@163.com>
2025-08-16 15:43:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.properties import StringProperty
 
-class MyApp(App):
-  def build(self):
-    return Button(text="Hello from Kivy")
+class DemoApp(App):
+    text_index = 0
+    words = ["Python", "Java", "C", "C++", "C#"]
 
-if __name__ == "__main__":
-  MyApp().run()
+    current_text = StringProperty(words[0])   # 绑定到 KV
+
+    def cycle_text(self):
+        self.text_index = (self.text_index + 1) % len(self.words)
+        self.current_text = self.words[self.text_index]
+
+if __name__ == '__main__':
+    DemoApp().run()
